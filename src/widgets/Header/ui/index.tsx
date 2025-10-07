986a401b56bd9bc24b22fa43c120ac6c9b59e8d3
Creciun <creciundorin25@gmail.com>
2025-10-07 21:46:26 +0300

    Add session-based logout functionality to Header widget.

--- src/widgets/Header/ui/index.tsx
+++ src/widgets/Header/ui/index.tsx
@@ -1,12 +1,23 @@
+import {useSessionStore} from "../../../entitites/session";
+import {logout} from "../../../features/auth/api/logout.ts";
+
 export const Header = () => {
+    const state = useSessionStore((s) => s.active)
     return (
         <header className="py-3.5 border-b border-gray-200">
             <div className="container flex justify-between items-center">
                 <span className={"text-2xl"}>Header</span>
 
-                <button className="">
-                    Login
-                </button>
+                {
+                    state
+                        ?   (<button onClick={logout}>
+                                Logout
+                            </button>)
+                        :   (<button>
+                            Login
+                            </button>)
+                }
+
             </div>
         </header>
     )
